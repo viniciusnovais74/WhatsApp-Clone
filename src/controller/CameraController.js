6a60b302a42eb52a7dccd1718cc826e2d90761be
Vinicius Novais <45321839+viniciusnovais74@users.noreply.github.com>
2021-12-06 21:01:19 -0300

    Arquivo Concluido

--- src/controller/CameraController.js
+++ src/controller/CameraController.js
@@ -1,15 +1,16 @@
+/**
+ * Situação do Arquivo: Completo
+ */
 export class CameraController {
 
     constructor(videoEl) {
 
         this._videoEl = videoEl;
 
-        navigator.mediaDevices.getUserMedia({
-            video: true
-        }).then(stream => {
+        navigator.mediaDevices.getUserMedia({ video: true }).then(stream => {
 
             this._stream = stream;
-            this._videoEl.srcObject = new MediaStream(stream);
+            this._videoEl.srcObject = windowURL.createObjectURL(stream);
             this._videoEl.play();
 
         }).catch(err => {
@@ -21,29 +22,29 @@ export class CameraController {
     }
 
     stop() {
-       
-        this._stream.getTracks().forEach(tracks => {
-       
-            tracks.stop()
-       
-        })
-    
+
+        this._stream.getTracks().forEach(track => {
+
+            track.stop();
+
+        });
+
     }
 
-    takePicture(mimetype = 'image/png') {
+    takePicture(mimeType = 'image/png') {
 
         let canvas = document.createElement('canvas');
-    
-        canvas.setAttribute('height',this._videoEl.videoHeight);
+
+        canvas.setAttribute('height', this._videoEl.videoHeight);
         canvas.setAttribute('width', this._videoEl.videoWidth);
 
         let context = canvas.getContext('2d');
 
         context.drawImage(this._videoEl, 0, 0, canvas.width, canvas.height);
 
-        return canvas.toDataURL(mimetype);
+        return canvas.toDataURL(mimeType);
 
     }
-    
+
 
 }
